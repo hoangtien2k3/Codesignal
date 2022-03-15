d363fa6f2745f3dfdc65574200d9c4216d9a0736
Hoàng Tiến <hoangtien2k3qx1@gmail.com>
2022-03-16 01:38:29 +0700

    Codesignal

---
+++ The_Core/15_WillYou.c
@@ -0,0 +1,21 @@
+
+/*
+    Codesignal\thecore\15_WillYou?
+*/
+
+#include<stdio.h>
+#include<stdbool.h>
+
+bool solution(bool young, bool beautiful, bool loved) {
+    // if((young && beautiful) && !loved)
+    //     return true;
+    // else if(loved &&(!young||!beautiful))
+    //     return  true;
+    // return false;
+    return (young && beautiful) != loved;
+}
+
+int main() {
+    bool young, beautiful, loved;
+    scanf ("")
+}
