bbe33f3ebc7c078ea8b8dc5b7e7a4b7c07a66454
Hoàng Tiến <hoangtien2k3qx1@gmail.com>
2022-03-15 08:22:55 +0700

    12_Codesignal/Thecore

---
+++ The_Core/11_ExtraNumber.c
@@ -0,0 +1,22 @@
+
+/*
+    You're given three integers, a, b and c. It is guaranteed that two of these 
+    integers are equal to each other. What is the value of the third integer?
+
+    Ex: For a = 2, b = 7, and c = 2, the output should be
+        solution(a, b, c) = 7.
+
+*/
+#include<stdio.h>
+
+int solution (int a, int b, int c) {
+    return (a==b)?c:(a==c)?b:a;
+    // return a^b^c;
+}
+
+int main() {
+    int a, b, c;
+    scanf ("%d%d%d", &a, &b, &c);
+    printf ("%d", solution(a, b, c));
+    return 0;
+}
---
+++ The_Core/12_IsInfiniteProcess.c
@@ -0,0 +1,33 @@
+
+/*
+    Given integers a and b, determine whether the following pseudocode results in an infinite loop
+
+    + while a is not equal to b do
+        increase a by 1
+        decrease b by 1
+
+    . For a = 2 and b = 6, the output should be
+    solution(a, b) = false;
+    . For a = 2 and b = 3, the output should be
+    solution(a, b) = true.
+
+*/
+
+#include<stdio.h>
+#include<stdbool.h>
+
+bool solution(int a, int b)
+{
+    return (b<a)?true:((b-a)%2)?false:true;
+}
+
+int main() {
+    int a, b;
+    scanf ("%d%d", &a, &b);
+    if (solution(a, b)) {
+        printf ("true");
+    } else {
+        printf ("false");
+    }
+    return 0;
+}
